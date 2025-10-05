7ccc016f6acfedafd4d990f01554661bdcba0ca7
selinbsa <selinbasakagan@gmail.com>
2025-10-05 16:42:52 +0300

    Changing dice.jsx

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-
 export default function Dice({ number, rolling }) {
-  const dicePath = `/src/assets/dice${number}.png`;
+  const dicePath = new URL(`../assets/dice${number}.png`, import.meta.url).href;
+
   return (
     <div className={`dice ${rolling ? "shake" : ""}`}>
       <img src={dicePath} alt={`Dice ${number}`} />
